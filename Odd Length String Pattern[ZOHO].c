31923db968cb79c32c1b1efb96c327f06f39f7ca
PRASANNA KUMAR R <114893414+GREAT-REBELS@users.noreply.github.com>
2023-09-27 20:57:45 +0530

    Update Odd Length String Pattern[ZOHO].c

--- Odd Length String Pattern[ZOHO].c
+++ Odd Length String Pattern[ZOHO].c
@@ -1,1 +1,2 @@
-An odd length string S is passed as the input.
+An odd length string S is passed as the input.The Program must print the pattern starting from the middle letter as shown in the Example 
+Input/Output section.
