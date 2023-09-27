e3e49d17300a25a0b82bd121e2c86a8b857f9fbb
PRASANNA KUMAR R <114893414+GREAT-REBELS@users.noreply.github.com>
2023-09-27 21:05:07 +0530

    Update Odd Length String Pattern[ZOHO].c

--- Odd Length String Pattern[ZOHO].c
+++ Odd Length String Pattern[ZOHO].c
@@ -1,2 +1,56 @@
 An odd length string S is passed as the input.The Program must print the pattern starting from the middle letter as shown in the Example 
 Input/Output section.
+  
+Example Input/Output 1:                                  
+Input:
+WATER
+Output:
+****T
+***TE
+**TER
+*TERW
+TERWA
+
+Example Input/Output 2:
+Input:
+ABCDEFG
+Output:
+******D
+*****DE
+****DEF
+***DEFG
+**DEFGA
+*DEFGAB
+DEFGABC
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#include <stdio.h>
+#include<string.h>
+int main() {
+char str[101];
+scanf("%s",str);
+int end=strlen(str)-1;
+for(int i=0;i<strlen(str);i++)
+{
+    int ind = strlen(str)/2;
+    for(int j=0;j<strlen(str);j++)
+    {
+        if(j>=end)
+        {
+            printf("%c",str[ind]);
+            ind++;
+            if(ind>=strlen(str))
+            {
+                ind = 0;
+            }
+        }
+        else
+        {
+            printf("*");
+        }
+    }
+    end--;
+    printf("\n");
+}
+}
+
+  
