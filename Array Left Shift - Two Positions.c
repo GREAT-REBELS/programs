8ce616e23a407f769bfe8d6a7e17509c3aac8d5f
PRASANNA KUMAR R <114893414+GREAT-REBELS@users.noreply.github.com>
2024-01-01 18:34:03 +0530

    Update Array Left Shift - Two Positions.c

--- Array Left Shift - Two Positions.c
+++ Array Left Shift - Two Positions.c
@@ -1,3 +1,27 @@
+The program must accept array of N integers as input. The program must shift the integers that occur between the positions X and Y (both inclusive) to the left by R times. The values X,Y and R also passed as 
+the input. The program must print the revised array as the output,
+
+Example Input/Output 1:
+Input:
+10
+10 20 30 40 50 60 70 80 90 100
+3 8
+2
+Output:
+10 20 50 60 70 80 30 40 90 100
+Explanation:
+Here X = 3, Y = 8 and R = 2.
+After 1 1st left shift, the array becomes 10 20 40 50 60 70 80 30 90 100.
+After 2nd left shift, the array becomes 10 20 50 60 70 80 30 40 90 100.
+
+Example Input/Output 2:
+Input:
+5
+60 70 80 90
+2 5 3
+Output:
+60 100 70 80 90
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #include <stdio.h>
 int main()
 {
