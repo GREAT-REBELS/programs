9beda6f1aeacdeab136f6799098dd2f8409db945
PRASANNA KUMAR R <114893414+GREAT-REBELS@users.noreply.github.com>
2024-01-03 09:29:59 +0530

    Create Largest Possible Two Digit Odd Integer.c

---
+++ Largest Possible Two Digit Odd Integer.c
@@ -0,0 +1,23 @@
+The program must accept two integers X and Y as the input. The program must choose exactly one digit from the digits of X and exactly one digit from the digits of Y. The program must form the largest possible
+two-digit odd integer T from the choosen digits. If it is not possible to form T, the program must print -1 as the output.
+
+Example Input/Output 1:
+Input: 1
+684 3457
+Output: 
+87
+Explanation: All possible two-digit odd integers are 13, 15, 17, 63, 65, 67, 83, 85, 87, 43, 45, 47, 31, 41, 51 and 71.
+Here the largest possible two-digit odd integer is 87. Hence the output is 87
+
+Example Input/Output 2:
+Input:
+48 802
+Output:
+-1
+
+Example Input/Output 3:
+Input:
+9088 296
+Output:
+99
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
