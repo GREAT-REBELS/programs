e8adec0977f47095371648d1d965556502a533e2
PRASANNA KUMAR R <114893414+GREAT-REBELS@users.noreply.github.com>
2024-01-06 13:55:09 +0530

    Update and rename .c to Distinct Length Pairs.c

--- Distinct Length Pairs.c
+++ Distinct Length Pairs.c
@@ -27,3 +27,33 @@ Input:
 Output:
 34
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+int N,M;
+scanf("%d %d",&N,&M);
+long int arr1[N],arr2[M];
+for(int i=0;i<N;i++)
+{
+    scanf("%ld",&arr1[i]);
+}
+for(int i=0;i<M;i++)
+{
+    scanf("%ld",&arr2[i]);
+}
+int cnt=0;
+for(int i=0;i<N;i++)
+{
+    int cnt1=log10(arr1[i])+1;
+    for(int j=0;j<M;j++)
+    {
+        int cnt2=log10(arr2[j])+1;
+        if(cnt1!=cnt2)
+        {
+            cnt++;
+        }
+    }
+}
+printf("%d",cnt);
+}
