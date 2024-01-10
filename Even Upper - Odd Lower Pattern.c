775209ca79568bfa0268f301afecedb95a38c4fb
PRASANNA KUMAR R <114893414+GREAT-REBELS@users.noreply.github.com>
2024-01-10 10:01:02 +0530

    Update and rename .c to Even Upper - Odd Lower Pattern.c

---
+++ Even Upper - Odd Lower Pattern.c
@@ -0,0 +1,15 @@
+The program must accept an integer N as the input. The program must print the desired pattern as shown in the Example Input/Output section.
+
+Example Input/Output 1:
+Input:
+4
+Output:
+
+Example Input/Output 2:
+Input:
+7
+Output:
+
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
