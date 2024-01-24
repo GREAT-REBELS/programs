cfaebd76ca0adadb7fbb72c7fbf5ed31e1ec0403
PRASANNA KUMAR R <114893414+GREAT-REBELS@users.noreply.github.com>
2024-01-24 11:03:19 +0530

    Create String - Replace by Asterisks Pattern.c

---
+++ String - Replace by Asterisks Pattern.c
@@ -0,0 +1,15 @@
+The program must accept an integer N and a string S containing only alphabets as the input.The program must print the desired pattern as shown in the Example Input/Output section.
+
+Example Input/Output 1:
+Input:
+3
+SkillRack
+Output:
+
+
+Example Input/Output 2:
+Input:
+5
+keyboard
+Output:
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
