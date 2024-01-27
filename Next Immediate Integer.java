b134b87e558ef82c49a623e8ea320fd756620498
PRASANNA KUMAR R <114893414+GREAT-REBELS@users.noreply.github.com>
2024-01-27 18:39:13 +0530

    Update Next Immediate Integer.java

--- Next Immediate Integer.java
+++ Next Immediate Integer.java
@@ -27,9 +27,9 @@ public class Main {
     String limit = "9".repeat(str.length());
     boolean flg = false;
     char arr1[] = str.toCharArray();
+    Arrays.sort(arr1);
     for (int i = Integer.valueOf(str) + 1; i <= Integer.valueOf(limit) + 1; i++) {
       char arr2[] = String.valueOf(i).toCharArray();
-      Arrays.sort(arr1);
       Arrays.sort(arr2);
       if (Arrays.equals(arr1, arr2)) {
         System.out.print(i);
