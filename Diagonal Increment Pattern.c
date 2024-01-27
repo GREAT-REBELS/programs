ed35b14346a121a0cabcb109dfe5986cc781e4ad
PRASANNA KUMAR R <114893414+GREAT-REBELS@users.noreply.github.com>
2024-01-27 17:58:39 +0530

    Create Diagonal Increment Pattern.c

---
+++ Diagonal Increment Pattern.c
@@ -0,0 +1,43 @@
+The program must accept an integer N as the input. The program must print the desired pattern as shown in the Example Input/Output section.
+
+Example Input/Output 1:
+Input:
+6
+Output:
+1 * * * * 2
+* 3 * * 4 *
+* * 5 6 * *
+* * 7 8 * *
+* 9 * * 10 *
+11 * * * *  12
+Example Input/Output 2:
+Input:
+5
+Output:
+1 * * * 2
+* 3 * 4 *
+* * 5 * *
+* 6 * 7 *
+8 * * * 9
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#include <stdio.h>
+
+int main() {
+  int N;
+  scanf("%d", & N);
+  int k = 1, strt = 0, end = N - 1;
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      if (j == strt) {
+        printf("%d ", k++);
+      } else if (j == end) {
+        printf("%d ", k++);
+      } else {
+        printf("* ");
+      }
+    }
+    strt++;
+    end--;
+    printf("\n");
+  }
+}
