6cca0ce55400a301e3e562d71cc6fc010ebb384f
PRASANNA KUMAR R <114893414+GREAT-REBELS@users.noreply.github.com>
2024-05-03 08:54:55 +0530

    Update Array to Diagonals Pattern.py

--- Array to Diagonals Pattern.py
+++ Array to Diagonals Pattern.py
@@ -6,6 +6,51 @@ Boundary Condition(s): 2 <= N <=50 1 <= Each integer value <= 10^8
 Input Format: The first line contains N. The second line contains N integers separated by a space.
 
 Output Format: The first N lines containing the modified matrix.
+
+Example Input/Output 1:
+Input:
+5
+125 5 10 788 99
+Output:
+125******99
+***5**788**
+****10*****
+***5**788**
+125******99
+
+Explanation:
+
+Here N = 5.
+
+The generated matrix with the 5 integers is given below.
+
+125 5 10 788 99
+125 5 10 788 99
+125 5 10 788 99
+125 5 10 788 99
+125 5 10 788 99
+      
+After replacing each digit with an asterisk in the non-diagonal integers of the matrix, it becomes
+125 * ** *** 99
+*** 5 ** 788 **
+*** * 10 *** **
+*** 5 ** 788 **
+125 * ** *** 99
+Finally, the matrix is printed with no space in each row. 
+
+Example Input/Output 2:
+Input:
+8
+67 3 166 81 44 553 55 9
+Output:
+67*************9
+**3**********55*
+***166****553***
+******8144******
+******8144******
+***166****553***
+**3**********55*
+67*************9
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 n=int(input())
 lst=list(map(int,input().split()))
