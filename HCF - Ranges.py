d468cd37b85cff41e22def53a19b2d72b2be25e4
PRASANNA KUMAR R <114893414+GREAT-REBELS@users.noreply.github.com>
2024-05-31 10:11:21 +0530

    Create HCF - Ranges.py

---
+++ HCF - Ranges.py
@@ -0,0 +1,33 @@
+The program must accept N integers and T ranges as the input. For each range(X, Y), the program must print the HCF of the integers from the Xth integer to the yth integer among the N integers as the output.
+
+Example Input/Output 1:
+
+Input:
+5 3
+2 30 10 12 20 
+1 5 
+2 3 
+3 5
+Output:
+2
+10
+2
+Explanation:
+In the first range (1, 5), the HCF of the integers 2 30 10 12 20 is 2. So 2 is printed. In the second range (2, 3), the HCF of the integers 30 10 is 10. So 10 is printed. In the third range (3, 5), the HCF 
+of the integers 10 12 20 is 2. So 2 is printed.
+  
+Example Input/Output 2:
+
+Input:
+6 4
+6 27 30 9 15 10
+2 4
+3 6
+5 6
+1 2
+Output:
+3
+1
+5
+3
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
