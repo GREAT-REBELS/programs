40a4e05b3d2c99c76d76449a8b515356a12420ea
PRASANNA KUMAR R <114893414+GREAT-REBELS@users.noreply.github.com>
2024-07-02 10:40:29 +0530

    Create String Common Words.py

---
+++ String Common Words.py
@@ -0,0 +1,30 @@
+The program must accept two space separated string values S1, S2 and an integer N as the input. The program must print the common words of size N in both the string values based on their order of occurrence in 
+S1 as the output. If the common words are repeated more than once in both the string values, the program must print such words only once. If there are no such common words then the program must print -1 as the 
+output.
+
+Example Input/Output 1:
+Input:
+He wore a turban made of gold 
+He was a young man of great promise
+2
+Output: 
+He of
+Explanation:
+The common words of size 2 in both the string values are He and of. Hence the output is He of.
+
+Example Input/Output 2:
+Input:
+the cat is under the bed 
+the Cat is beside the table
+3
+Output:
+the
+
+Example Input/Output 3:
+Input:
+If you want to build a solid structure 
+you need to put down the first few blocks in the right place
+4
+Output
+-1
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
