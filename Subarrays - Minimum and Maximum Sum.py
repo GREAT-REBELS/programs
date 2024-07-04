567432bd9c45904f000dcd67ce9388433454ae72
PRASANNA KUMAR R <114893414+GREAT-REBELS@users.noreply.github.com>
2024-07-04 12:03:44 +0530

    Update Subarrays - Minimum and Maximum Sum.py

--- Subarrays - Minimum and Maximum Sum.py
+++ Subarrays - Minimum and Maximum Sum.py
@@ -28,24 +28,21 @@ arr = list(map(int, input().split()))
 K = int(input())
 
 left, right = 0, K
-minInd1, minInd2 = 0, K
-maxInd1, maxInd2 = 0, K
-subArr_sum = sum(arr[:K])
+minInd,maxInd = left,left
+subArr_sum = sum(arr[left:right])
 min_sum, max_sum = subArr_sum, subArr_sum
 
-while right < N:
+while left < N-K:
     subArr_sum += arr[right] - arr[left]
     left += 1
     right += 1
     if subArr_sum > max_sum:
         max_sum = subArr_sum
-        maxInd1 = left
-        maxInd2 = right
+        maxInd = left
 
     if subArr_sum < min_sum:
         min_sum = subArr_sum
-        minInd1 = left
-        minInd2 = right
+        minInd = left
 
-print(*arr[minInd1:minInd2])
-print(*arr[maxInd1:maxInd2])
+print(*arr[minInd:minInd+K])
+print(*arr[maxInd:maxInd+K])
