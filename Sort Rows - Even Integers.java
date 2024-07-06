50ec285154540389d2318d057e13d9ed41277be2
PRASANNA KUMAR R <114893414+GREAT-REBELS@users.noreply.github.com>
2024-07-06 11:18:29 +0530

    Create Sort Rows - Even Integers.java

---
+++ Sort Rows - Even Integers.java
@@ -0,0 +1,37 @@
+The program must accept an integer matrix of size RxC as the input. The program must sort the rows of the matrix based on the number of even integers in each row. If more than one rows have the same number of even
+integers, the program must sort those rows in the order of their occurrence. Finally, the program must print the modified matrix as the output.
+
+Example Input/Output 1:
+Input:
+3 4
+1 2 4 5
+4 9 8 6
+9 7 8 5
+Output:
+9 7 8 5
+1 2 4 5
+4 9 8 6
+Explanation:
+The number of even integers in the 1st row of the matrix is 2 (1245).
+The number of even integers in the 2nd row of the matrix is 3 (4986). The number of even integers in the 3rd row of the matrix is 1 (9785).
+After sorting the rows of the matrix based on the number of even integers, the matrix becomes
+9 7 8 5
+1 2 4 5
+4 9 8 6
+
+Example Input/Output 2:
+Input:
+5 5
+84 48 94 18 79
+35 97 13 75 91
+99 48 53 37 99
+36 13 38 10 15
+93 55 25 94 41
+Output:
+35 97 13 75 91
+99 48 53 37 99
+93 55 25 94 41
+36 13 38 10 15
+84 48 94 18 79
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  
