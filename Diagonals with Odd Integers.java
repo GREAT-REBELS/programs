ba90f8066f0b0e87d95d34adb34e7b7f84ae3cf0
PRASANNA KUMAR R <114893414+GREAT-REBELS@users.noreply.github.com>
2024-07-08 11:29:07 +0530

    Create Diagonals with Odd Integers.java

---
+++ Diagonals with Odd Integers.java
@@ -0,0 +1,38 @@
+The program must accept an integer matrix of size RxC as the input. The program must print the number of diagonals having only odd integers that are parallel to the top-left to bottom-right diagonal and the 
+top-right to bottom-left diagonal in the matrix.
+
+Example Input/Output 1:
+Input
+5 5
+60 37 26 98 73
+29 50 32 52 43
+52 19 29 81 33
+13 94 17 63 80
+90 25 77 11 54
+Output:
+6
+Explanation:
+The diagonals that are parallel to the top-left to bottom-right diagonal and having only odd integers are highlighted below.
+60 37 26 98 73
+29 50 32 52 43
+52 19 29 81 33
+13 94 17 63 80
+90 25 77 11 54
+The diagonals that are parallel to the top-right to bottom-left diagonal and having only odd integers are highlighted below.
+60 37 26 98 73
+29 50 32 52 43
+52 19 29 81 33
+13 94 17 63 80
+90 25 77 11 54
+the total number of diagonals with only odd integers is 6. So 6 is printed as the output.
+
+Example Input/Output 2:
+Input:
+4 3
+354 234 259
+104 384 352
+331 289 127
+127 261 288
+Output: 
+4
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
