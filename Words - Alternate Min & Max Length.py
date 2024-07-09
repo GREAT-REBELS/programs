31e93c90acb7612394b96cc7ad0b4710a3add30c
PRASANNA KUMAR R <114893414+GREAT-REBELS@users.noreply.github.com>
2024-07-09 11:13:37 +0530

    Create Words - Alternate Min & Max Length.py

---
+++ Words - Alternate Min & Max Length.py
@@ -0,0 +1,27 @@
+The program must accept a string S containing multiple words as the input. The program must print the word which has the minimum length among the first word and the last word in S. Then the program must print the
+word which has the maximum length among the second word and the last but one word in S. Similarly, the program must print the remaining words alternatively. If the lengths of the two words are equal in comparison,
+then the program must print them in the order of their occurrence.
+Note: The string S always contains an even number of words.
+
+Example Input/Output 1:
+Input:
+A cup of tea
+Output:
+A cup
+Explanation:
+The given string is "A cup of tea".
+The word with the minimum length among the first word "A" and the last word "tea" is "A".
+The word with the maximum length among the second word "cup" and the last but one word "of" is "cup". Hence the output is A cup
+
+Example Input/Output 2:
+Input:
+God helps those who help themselves
+Output:
+God helps who
+
+Example Input/Output 3:
+Input:
+All good things must come to an end
+Output:
+All end good to must come
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
