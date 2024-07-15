dffb75e7658838d733d99d27320bc79523f564a3
PRASANNA KUMAR R <114893414+GREAT-REBELS@users.noreply.github.com>
2024-07-15 12:50:20 +0530

    Update Rotate Matrix by X Degrees.java

--- Rotate Matrix by X Degrees.java
+++ Rotate Matrix by X Degrees.java
@@ -35,3 +35,60 @@ Input:
 Output:
 INVALID
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+import java.util.Scanner;
+public class Main {
+  static void transposeMatrix(int mat[][]) {
+    int N = mat.length;
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        int temp = mat[i][j];
+        mat[i][j] = mat[j][i];
+        mat[j][i] = temp;
+      }
+    }
+  }
+  static void reverseMatrix(int mat[][]) {
+    int N = mat.length;
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N / 2; j++) {
+        int temp = mat[i][j];
+        mat[i][j] = mat[i][N - j - 1];
+        mat[i][N - j - 1] = temp;
+      }
+    }
+  }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int mat[][] = new int[N][N];
+
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        mat[i][j] = sc.nextInt();
+      }
+    }
+
+    int deg = sc.nextInt();
+
+    if (deg % 90 != 0) {
+      System.out.print("INVALID");
+      return;
+    }
+
+    int turns = deg / 90;
+
+    while (turns > 0) {
+      transposeMatrix(mat);
+      reverseMatrix(mat);
+      turns--;
+    }
+
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        System.out.print(mat[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+}
