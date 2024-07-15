0805eb366465530b93bc398a02683e12257bd7d2
PRASANNA KUMAR R <114893414+GREAT-REBELS@users.noreply.github.com>
2024-07-15 11:38:33 +0530

    Create Rotate Matrix by X Degrees.java

---
+++ Rotate Matrix by X Degrees.java
@@ -0,0 +1,37 @@
+The program must accept an integer matrix of size NxN and an integer X as the input. The program must rotate the matrix by X degrees in the clockwise direction. Finally, the program must print the modified matrix
+as the output. If it is not possible to rotate the matrix by X degrees, the program must print INVALID as the output. The matrix rotation is possible if the value of X is a multiple of 90 (i.e., 0, 90, 180, 270, 
+360, 450, 540, and so on).
+
+Example Input/Output 1:
+Input:
+4
+44 17 27 29
+39 29 18 19
+13 40 13 31
+36 47 29 22
+270
+Output:
+29 19 31 22
+27 18 13 29
+17 29 40 47
+44 39 13 36
+Explanation:
+After rotating the matrix by 270 degrees, the matrix becomes.
+29 19 31 22
+27 18 13 29
+17 29 40 47
+44 39 13 36
+
+
+Example Input/Output 2:
+Input:
+5 
+35 11 98 52 8 
+44 48 77 85 80 
+67 77 25 44 10 
+97 17 38 55 95 
+32 70 78 85 69
+45
+Output:
+INVALID
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
