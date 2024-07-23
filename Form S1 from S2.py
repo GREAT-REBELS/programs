320da2c4ca3e9a4790130bf4077952249401a44a
PRASANNA KUMAR R <114893414+GREAT-REBELS@users.noreply.github.com>
2024-07-23 06:03:28 +0530

    Create Form S1 from S2.py

---
+++ Form S1 from S2.py
@@ -0,0 +1,32 @@
+The program must accept two string values S1 and S2 and an integer X as the input. The program must print YES, if S1 can be formed from the alphabets of S2 for at least X times. Else the program must print NO as 
+the output.
+Note: The string values S1 and S2 are case sensitive. 
+
+Example Input/Output 1:
+Input:
+Laptop 
+lapoLptpAtpLoaks
+2
+Output:
+YES
+Explanation:
+Here X=2, the string S1 is Laptop and the string S2 is lapoLptpAtpLoaks.
+The string Laptop can be formed from the alphabets of lapoLptpAtpLoaks for 2 times.
+Hence the output is YES
+
+Example Input/Output 2:
+Input:
+CONTINENT 
+CpTkOiNqunptsNe
+1
+Output:
+NO
+
+Example Input/Output 3:
+Input:
+Abc#123
+a#cE#23pb1bA12ACLK13LAb#32cP
+3
+Output:
+YES
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
