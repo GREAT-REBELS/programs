425bdba7452ebd90ef7266b636a9692e2f46b6d9
PRASANNA KUMAR R <114893414+GREAT-REBELS@users.noreply.github.com>
2024-07-24 15:15:09 +0530

    Update and rename Replace Nearest Ring with 1s to Replace Nearest Ring with 1s.java

--- Replace Nearest Ring with 1s.java
+++ Replace Nearest Ring with 1s.java
@@ -80,3 +80,42 @@ Output:
 X 0 1 0
 0 0 0 0
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int R = sc.nextInt();
+    int C = sc.nextInt();
+    char[][] mat = new char[R][C];
+    int strtRow = 0, endRow = 0, strtCol = 0, endCol = 0;
+    for (int i = 0; i < R; i++) {
+      for (int j = 0; j < C; j++) {
+        mat[i][j] = sc.next().charAt(0);
+        if (mat[i][j] == 'X') {
+          strtRow = i - 1;endRow = i + 1;strtCol = j - 1;endCol = j + 1;
+        }
+      }
+    }
+    int flg = 0;
+    while (strtRow >= 0 && strtCol >= 0 && endRow < R && endCol < C) {
+      for (int i = strtRow; i <= endRow; i++) {
+        for (int j = strtCol; j <= endCol; j++) {
+          if (mat[i][j] == '0') {
+            mat[i][j] = '1';
+            flg = 1;
+          }
+        }
+      }
+      if (flg == 1) {
+        break;
+      }
+      strtRow -= 1;endRow += 1;strtCol -= 1;endCol += 1;
+    }
+    for (int i = 0; i < R; i++) {
+      for (int j = 0; j < C; j++) {
+        System.out.print(mat[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+}
