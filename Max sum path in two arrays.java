a6efd421e6ee429b04877359227719680b283d61
PRASANNA KUMAR R <114893414+GREAT-REBELS@users.noreply.github.com>
2024-09-01 09:53:51 +0530

    Update Max sum path in two arrays.java

--- Max sum path in two arrays.java
+++ Max sum path in two arrays.java
@@ -11,3 +11,31 @@ Input: arr1 = [1, 2, 3] , arr2[] = [3, 4, 5]
 Output: 15
 Explanation: The path will be 1+2+3+4+5=15.
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+class Solution {
+    public int maxPathSum(List<Integer> arr1, List<Integer> arr2) {
+        int n = arr1.size(),m = arr2.size();
+        int i=0,j=0;
+        int sum1 = 0,sum2=0,res = 0;
+        while(i<n && j<m){
+            if(arr1.get(i) < arr2.get(j)){
+                sum1+=arr1.get(i);i++;
+            }else if(arr1.get(i) > arr2.get(j)){
+                sum2+=arr2.get(j);j++;
+            }else{
+                res += Math.max(sum1,sum2) + arr1.get(i);
+                sum1 = 0;sum2 = 0;
+                i++;j++;
+            }
+        }
+        while(i<n){
+            sum1+=arr1.get(i);
+            i++;
+        }
+        while(j<m){
+            sum2+=arr2.get(j);
+            j++;
+        }
+        res += Math.max(sum1,sum2);
+        return res;
+    }
+}
